0ce1f5b5e48e1247df0c59b2c62a1b37667985a6
Bastien Potiron <bpo@makina-corpus.com>
2021-10-05 23:55:37 +0200

    Handle multiple languages for mails

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project is a small bot that send a daily email containing a goat picture to
 
 See the instructions below to install and use this project.
 
-### License
+## License
 
 This project uses some pictures taken form the database "Mendelay Data" : https://data.mendeley.com/datasets/4skwhnrscr/1
 
@@ -12,7 +12,7 @@ This dataset is under the CC BY 4.0 License and you should read the terms of thi
 
 Otherwise, the project itself is under the MIT License. You can reproduce, modify and do pretty much whatever you want with the code.
 
-### Instructions
+## Instructions
 
 To be able to use this project, you'll need to configure some elements into a `.env` file. To do so rename the file `.env.dist` to `.env` and complete the required informations.
 
@@ -22,6 +22,42 @@ You'll need to provide pictures to the program. To do so you should put your ima
 
 When your program is correctly configured, you'll just have to create a cron on your server in order for the program to run daily.
 
-### Warnings
+## Warnings
 
 Be sure that you have the consent from the recepients of the emails before using the program.
+
+## Internationalization
+
+The mailer system supports multiple languages. By default it send an email containing the values of MAIL_SUBJECT and MAIL_CONTENT. But you can define multiple languages by adding values into the array LANGUAGES. For each language of the array, you should define the following variables in your `.env` file :
+- MAIL_SUBJECT_{language_code}
+- MAIL_CONTENT_{language_code}
+
+you should also adapt you recipients list by creating multiple lists in the `mailing_list.py` file.
+
+### Example
+
+For example, if you want you bot to send a daily picture with content in English and French you need to have the following .env file :
+
+```
+LANGUAGES = ['en', 'fr']
+MAIL_SUBJECT_EN = "Title of my mail"
+MAIL_CONTENT_EN = "Content of my mail"
+MAIL_SUBJECT_FR = "Titre de mon mail"
+MAIL_CONTENT_FR = "Contenu de mon mail"
+MAIL_SENDER_EMAIL = "example@example.org"
+MAIL_SENDER_PASSWORD = "PassWord1234*!"
+PATH_TO_DATA_FOLDER = "data/"
+PATH_TO_OLD_DATA_FOLDER = "old/"
+```
+
+In your `mailing_list.py` file you'll define two arrays of recipients :
+
+```
+RECIPIENTS_EN = [
+    # Multiple emails
+]
+
+RECIPIENTS_FR = [
+    # Multiple emails
+]
+```
--- mailer.py
+++ mailer.py
@@ -9,56 +9,97 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from mailing_list import RECIPIENTS
+import mailing_list
 
-# Load local environment
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
 
-sender_email = os.environ.get('MAIL_SENDER_EMAIL', '')
-password = os.environ.get('MAIL_SENDER_PASSWORD', '')
-path_to_data = os.environ.get('PATH_TO_DATA_FOLDER', '')
-path_to_used_data = os.environ.get('PATH_TO_OLD_DATA_FOLDER', '')
+def retrieve_and_encode_attachment(filepath, filename):
+    # Open file in binary mode
+    with open(filepath, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["Subject"] = os.environ.get('MAIL_SUBJECT', "")
-message["Bcc"] = ", ".join(RECIPIENTS)
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
 
-# Add body to email
-message.attach(MIMEText(os.environ.get('MAIL_CONTENT', ""), "plain"))
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
 
-# Retrieve random file from folder data
-filename = random.choice(os.listdir(path_to_data))
-filepath = f"{path_to_data}/{filename}"
+    return part
 
-# Open file in binary mode
-with open(filepath, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
 
-# Encode file in ASCII characters to send by email
-encoders.encode_base64(part)
+def create_mail(subject, content, recipients, path_to_data, sender_email, filename, filepath):
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["Subject"] = subject
+    message["Bcc"] = ", ".join(recipients)
 
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
+    # Add body to email
+    message.attach(MIMEText(content, "plain"))
 
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
+    attachment = retrieve_and_encode_attachment(filepath, filename)
 
-# Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, RECIPIENTS, text)
+    # Add attachment to message and convert message to string
+    message.attach(attachment)
 
-# Move used picture to another folder to avoid using it again
-os.rename(f"{path_to_data}/{filename}", f"{path_to_used_data}/{filename}")
+    return message.as_string()
 
+
+def send_mails(sender_email, password, mails):
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        for mail in mails:
+            server.sendmail(
+                from_addr=sender_email,
+                to_addrs=mail[0],
+                msg=mail[1]
+            )
+
+
+def main():
+    # Load local environment
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    load_dotenv(os.path.join(basedir, '.env'))
+
+    sender_email = os.environ.get('MAIL_SENDER_EMAIL', '')
+    password = os.environ.get('MAIL_SENDER_PASSWORD', '')
+    path_to_data = os.environ.get('PATH_TO_DATA_FOLDER', '')
+    path_to_used_data = os.environ.get('PATH_TO_OLD_DATA_FOLDER', '')
+    languages = os.environ.get('LANGUAGES', "")
+    languages = [str(t[1:-1]) for t in languages[1:-1].split(', ') if t]  # Yep, that's ugly, but I'm tired so it'll do the job.
+
+    # Retrieve random file from folder data
+    filename = random.choice(os.listdir(path_to_data))
+    filepath = f"{path_to_data}/{filename}"
+
+    mails = []
+
+    if len(languages):
+        for lang in languages:
+            subject = os.environ.get(f'MAIL_SUBJECT_{lang.upper()}', "")
+            content = os.environ.get(f'MAIL_CONTENT_{lang.upper()}', "")
+            recipients = getattr(mailing_list, f"RECIPIENTS_{lang.upper()}")
+            text = create_mail(subject, content, recipients, path_to_data, sender_email, filename, filepath)
+            mails.append((recipients, text))
+
+    else:
+        subject = os.environ.get('MAIL_SUBJECT', "")
+        content = os.environ.get('MAIL_CONTENT', "")
+        recipients = getattr(mailing_list, "RECIPIENTS")
+        text = create_mail(subject, content, recipients, path_to_data, sender_email, filename, filepath)
+        mails.append((recipients, text))
+
+    send_mails(sender_email, password, mails)
+
+    # Move used picture to another folder to avoid using it again
+    os.rename(f"{path_to_data}/{filename}", f"{path_to_used_data}/{filename}")
+
+
+main()
